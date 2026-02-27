2fed0fc8616991bb48849c7d83a811ca3215c6d7
Gaaab.14 <cargabmencar@gmail.com>
2026-02-26 21:14:19 -0500

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.ksp)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,4 +51,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
 }
